456649fb7ed7a9bf1d69a4fb3ae09118a6601f5b
nader2356 <naderabdessaied8@gmail.com>
2023-09-06 22:02:04 +0200

    connect mongodb to database

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from "./src/routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern_lovie_2023").then(() => {
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
+});
