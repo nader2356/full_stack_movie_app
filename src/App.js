5d6b5e464b28ca8a173a981914cf70c5da7cef70
nader2356 <naderabdessaied8@gmail.com>
2023-09-14 22:27:07 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import themeConfigs from "./config/theme.config";
+import { ToastContainer } from "react-toastify";
+import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import routes from "./routes/routes";
+import PageWrapper from "./components/common/PageWrapper";
 
-function App() {
+import "react-toastify/dist/ReactToastify.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* config toastify */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* mui reset css */}
+      <CssBaseline />
+
+      {/* app routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, index) => (
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={route.state ? (
+                    <PageWrapper state={route.state}>{route.element}</PageWrapper>
+                  ) : route.element}
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={route.state ? (
+                    <PageWrapper state={route.state}>{route.element}</PageWrapper>
+                  ) : route.element}
+                />
+              )
+            ))}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* app routes */}
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
