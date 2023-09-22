2abbd49989f3a28e781c30be3e4bd2565d522f71
nader2356 <naderabdessaied8@gmail.com>
2023-09-22 20:57:58 +0200

    add NavigationSwiper component in react app

--- src/component/common/TopBar.js
+++ src/component/common/TopBar.js
@@ -29,12 +29,15 @@ const ScrollAppBar = ({ children, window }) => {
     }
   });
 };
-const TopBar = () => {
-
+const Topbar = () => {
+  const { user } = useSelector((state) => state.user);
   const { appState } = useSelector((state) => state.appState);
   const { themeMode } = useSelector((state) => state.themeMode);
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
   const dispatch = useDispatch();
+
   const onSwithTheme = () => {
     const theme = themeMode === themeModes.dark ? themeModes.light : themeModes.dark;
     dispatch(setThemeMode(theme));
@@ -93,9 +96,14 @@ const TopBar = () => {
 
             {/* user menu */}
             <Stack spacing={3} direction="row" alignItems="center">
-             
+              {!user && <Button
+                variant="contained"
+                onClick={() => dispatch(setAuthModalOpen(true))}
+              >
+                sign in
+              </Button>}
             </Stack>
-       
+            {user && <UserMenu />}
             {/* user menu */}
           </Toolbar>
         </AppBar>
@@ -104,4 +112,4 @@ const TopBar = () => {
   );
 };
 
-export default TopBar;
+export default Topbar;
